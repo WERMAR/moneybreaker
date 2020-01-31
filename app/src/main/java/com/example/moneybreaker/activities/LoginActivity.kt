9ee6d322a9855cf1a57022c76a001fd8f4739c18
wermar <marcel.wernisch@outlook.de>
2020-01-31 22:54:19 +0100

    add login activity with screen and do first setup for multi-lang app

--- app/src/main/java/com/example/moneybreaker/activities/LoginActivity.kt
+++ app/src/main/java/com/example/moneybreaker/activities/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.example.moneybreaker
+package com.example.moneybreaker.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moneybreaker.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/example/moneybreaker/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.moneybreaker.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moneybreaker.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
