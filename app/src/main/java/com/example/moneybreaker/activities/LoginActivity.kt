e28e71a3f323e391f2a7b6283ea609b4aa9085c0
wermar <marcel.wernisch@outlook.de>
2020-02-12 04:18:35 +0100

    commit functionality of login

--- app/src/main/java/com/example/moneybreaker/activities/LoginActivity.kt
+++ app/src/main/java/com/example/moneybreaker/activities/LoginActivity.kt
@@ -1,13 +1,31 @@
 package com.example.moneybreaker.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.moneybreaker.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        setupButtons()
+    }
+
+    /**
+     * this method bind all ui buttons to the functionality methods
+     */
+    private fun setupButtons() {
+        loginButton.setOnClickListener { login() }
+    }
+
+    /**
+     * this method check if the user access exist and do the login of the user
+     */
+    private fun login() {
+        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+
     }
 }
