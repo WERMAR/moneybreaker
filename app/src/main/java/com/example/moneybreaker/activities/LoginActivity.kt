0d10a801fd89d051e80ea277fac0e805ddbc172e
wermar <marcel.wernisch@outlook.de>
2020-02-29 00:33:09 +0100

    - add new activity "LoginActivity" - add new activity "RegistrationActivity"

--- app/src/main/java/com/example/moneybreaker/activities/LoginActivity.kt
+++ app/src/main/java/com/example/moneybreaker/activities/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.example.moneybreaker.activities
 
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import com.example.moneybreaker.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -19,6 +23,7 @@ class LoginActivity : AppCompatActivity() {
      */
     private fun setupButtons() {
         loginButton.setOnClickListener { login() }
+        registrationButton.setOnClickListener { registration() }
     }
 
     /**
@@ -27,5 +32,15 @@ class LoginActivity : AppCompatActivity() {
     private fun login() {
         startActivity(Intent(this@LoginActivity, MainActivity::class.java))
 
+    }
+
+    private fun registration() {
+        val vibrator : Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            vibrator.vibrate(VibrationEffect.createOneShot(150, VibrationEffect.DEFAULT_AMPLITUDE))
+        } else {
+            vibrator.vibrate(150)
+        }
+        startActivity(Intent(this@LoginActivity, RegistrationActivity::class.java))
     }
 }
